daf79ac439993fd8164e6d5e006d742750cc2487
Juanes-xd <juanestebanortizbejarano@gmail.com>
2024-05-02 01:07:39 -0500

    add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-
-import './App.css'
-
 function App() {
- 
-
   return (
-    <h1 className='text-3xl font-bold underline' >
-      Hello World!
-    </h1>
-  )
+    <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+      <div className="bg-gray-950 p-4 w-2/5">
+        <h1>Task</h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
